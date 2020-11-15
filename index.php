53c9e497b4b38e07b3af8abf25ccd0fe10d9a892
Saddam <akhmedkhanov.sa@gmail.com>
2020-11-15 17:07:42 +0300

    add validate and logging

--- index.php
+++ index.php
@@ -1,26 +1,65 @@
 <?php
-
-$temp = [
-    ['Sad', 'Akh', 'test@mail.ru', '222', '333'],
-    ['Sad', 'Akh', 'test2@mail.ru', '222', '333'],
-    ['Sad', 'Akh', 'test3@mail.ru', '222', '333'],
-    ['Sad', 'Akh', 'test4@mail.ru', '222', '333'],
-    ['Sad', 'Akh', 'test5@mail.ru', '222', '333'],
+date_default_timezone_set("Europe/Moscow");
+$arrayUsers = [
+    [
+        'id' => 1,
+        'name' => 'Sad1',
+        'email' => 'test1@mail.ru'
+    ],
+    [
+        'id' => 2,
+        'name' => 'Sad2',
+        'email' => 'test2@mail.ru'
+    ],
+    [
+        'id' => 3,
+        'name' => 'Sad3',
+        'email' => 'test3@mail.ru'
+    ],
+    [
+        'id' => 4,
+        'name' => 'Sad4',
+        'email' => 'test4@mail.ru'
+    ],
+    [
+        'id' => 5,
+        'name' => 'Sad5',
+        'email' => 'test5@mail.ru'
+    ]
 ];
+
+function writeLog($str) {
+    $log = date('Y-m-d H:i:s', time()) . $str;
+    file_put_contents(__DIR__ . '/log.txt', $log . PHP_EOL, FILE_APPEND);
+}
+
+$email = $_POST['email'];
+if(!strpos($email, '@')) {
+    writeLog(" Введена некорректная почта $email (обошли валидацию на фронте)");
+    echo "Error: incorrect email";
+    exit();
+}
+
+if($_POST['password'] != $_POST['retryPassword']) {
+    writeLog(" Пароли не совпадают. (обошли валидацию на фронте)");
+    echo "Error: passwords don't match";
+    exit();
+}
 
 $flag = TRUE;
 
-foreach($temp as $item) {
-    if(in_array($_POST['email'], $item)) {
+foreach($arrayUsers as $user) {
+    if($email == $user['email']) {
         $flag = FALSE;
     }
 }
 
 if($flag) {
+    writeLog(" Успешная отправка формы с почтой $email");
     echo "OK";
+    exit();
 } else {
+    writeLog(" Учетная запись с почтой $email уже существует");
     echo "Error";
+    exit();
 }
-
-
-//echo json_encode($temp);
--- script.js
+++ script.js
@@ -1,29 +1,58 @@
-const buttonSend = $('.btnSend');
+$('.btnSend').on('click', () => {
+    let validateFlag = true;
 
-buttonSend.on('click', () => {
-    const data = {
-        firstName: $("input[name='firstName']").val(),
-        lastName: $("input[name='lastName']").val(),
-        email: $("input[name='email']").val(),
-        password: $("input[name='password']").val(),
-        retryPassword: $("input[name='retryPassword']").val(),
+    $('.notificationError').remove(); // удаляем все выводы ошибок, перепроверяем и отрисовываем снова
+
+    if( $("input[name='email']").val().indexOf('@') == -1 ) { // проверяем корректность ввода почты
+        validateFlag = false;
+        $('.form').before('<div class="alert alert-danger text-center notificationError incorrectEmail"> Некорректный ввод почты </div>');
     }
 
-    $.ajax({
-        method: 'POST',
-        url: "index.php",
-        data,
-        success: (data) => {
-            console.log(data)
+    if( $("input[name='password']").val() != $("input[name='retryPassword']").val() ) { // проверяем пароли
+        validateFlag = false;
+        $('.form').before('<div class="alert alert-danger text-center notificationError passwordMatch"> Пароли не совпадают </div>');
+    }
 
-            if( data == 'OK' ) {
-                $('.form').addClass('d-none');
-                $('.notificationError').addClass('d-none');
-                $('.container').append('<div class="alert alert-success text-center"> Вы успешно отправили форму </div>');
-                $('.container').append('<div class="text-center"><a href="index.html" class="btn btn-success align-items-center"> Вернуться </a></div>');
-            } else if( data == 'Error' && !$('div').is('.notificationError') ) {
-                $('.form').before('<div class="alert alert-danger text-center notificationError"> Пользователь с такой почтой уже существует </div>')
-            }
+    if(validateFlag) {
+        const data = {
+            firstName: $("input[name='firstName']").val().trim(),
+            lastName: $("input[name='lastName']").val().trim(),
+            email: $("input[name='email']").val().trim(),
+            password: $("input[name='password']").val().trim(),
+            retryPassword: $("input[name='retryPassword']").val().trim(),
         }
-    })
+
+        $.ajax({
+            method: 'POST',
+            url: "index.php",
+            data,
+            success: (data) => {
+                console.log(data)
+    
+                switch(data) { // в зависимости от ответа выводим ошибку
+                    case "OK":
+                        $('.form').addClass('d-none');
+                        $('.notificationError').addClass('d-none');
+                        $('.container').append('<div class="alert alert-success text-center"> Вы успешно отправили форму </div>');
+                        $('.container').append('<div class="text-center"><a href="index.html" class="btn btn-success align-items-center"> Вернуться </a></div>');
+                        break;
+                    case "Error":
+                        if(!$('div').is('.emailExists')) {
+                            $('.form').before('<div class="alert alert-danger text-center notificationError emailExists"> Пользователь с такой почтой уже существует </div>');
+                        }
+                        break;
+                    case "Error: incorrect email":
+                        if(!$('div').is('.incorrectEmail')) {
+                            $('.form').before('<div class="alert alert-danger text-center notificationError incorrectEmail"> Некорректный ввод почты </div>');
+                        }
+                        break;
+                    case "Error: passwords don't match":
+                        if(!$('div').is('.passwordMatch')) {
+                            $('.form').before('<div class="alert alert-danger text-center notificationError passwordMatch"> Пароли не совпадают </div>');
+                        }
+                        break;
+                }
+            }
+        })
+    }
 })
